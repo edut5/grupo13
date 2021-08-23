85c00d2421a5fba08688a10473be48c6ab5430ad
alediaz1985-pc <alediaz1985@hotmail.com>
2021-08-23 08:10:51 -0300

    Inicio del Proyecto, se creo carpetas (apps, media, static, templates)

---
+++ Sitio/Sitio/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def inicio(request):
+	template_name="inicio.html"
+	ctx={}
+	return render(request, template_name, ctx)
+
+
+def login(request):
+	template_name="login.html"
+	ctx={}
+	return render(request, template_name, ctx)
